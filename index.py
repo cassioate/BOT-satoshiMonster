e17b2275a24ad727acb28cd1a871c7ea08e9cf98
Cassio <cassioate@gmail.com>
2022-02-02 20:08:50 -0300

    pt-pt

--- index.py
+++ index.py
@@ -8,6 +8,8 @@ dotenv.load_dotenv()
 def conectarFunc():
     connect = True
     login = os.getenv("EMAIL")
+    caracter = pyautogui.hotkey('alt','ctrl', '2')
+    emaildominio = os.getenv("EMAILDOMINIO")
     senha = os.getenv("SENHA")
     pyautogui.keyDown("ctrl")
     pyautogui.press("f5")
@@ -18,7 +20,7 @@ def conectarFunc():
         if (procurarImagemSemRetornarErro("login")):
             x, y = procurarLocalizacaoDaImagemPelosEixos("email")
             pyautogui.click(x, y+30, duration=3)
-            pyautogui.write(login)
+            pyautogui.write(login+caracter+emaildominio)
             x, y = procurarLocalizacaoDaImagemPelosEixos("password")
             pyautogui.click(x, y+30, duration=3)
             pyautogui.write(senha)
